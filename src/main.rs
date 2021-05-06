ce016caf3f0ec6aec08ff1878426d8eb99ff779b
Paul <paulmakles@gmail.com>
2021-05-06 20:42:32 +0100

    Parse metadata and throw it out.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use actix_web::{
 };
 use reqwest::header::CONTENT_TYPE;
 use scraper::{Html, Selector};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 pub mod util;
 use util::result::Error;
@@ -28,45 +28,44 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-/*#[derive(Deserialize)]
-struct Info {
-    url: String,
-}
-
-async fn embed(info: Query<Info>) -> impl Responder {
-    let url = info.into_inner().url;
-
-    let resp = reqwest::get(url).await.unwrap();
-    assert!(resp.status().is_success());
-
-    dbg!(resp.headers().get(CONTENT_TYPE));
-
-    let body = resp.text().await.unwrap();
-    let fragment = Html::parse_document(&body);
-    let selector = Selector::parse("meta").unwrap();
-
-    for el in fragment.select(&selector) {
-        let node = el.value();
-        dbg!(node.attr("property"));
-        dbg!(node.attr("content"));
-    }
-
-    format!("gaming has been deposited into stdout")
-}*/
-
 #[derive(Deserialize)]
 struct Parameters {
     url: String,
 }
 
+#[derive(Debug, Serialize)]
+struct Media {
+    url: String,
+    width: isize,
+    height: isize
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "type")]
+enum Image {
+    Preview(Media),
+    Large(Media)
+}
+
+#[derive(Default, Debug, Serialize)]
 struct Metadata {
     title: Option<String>,
+    description: Option<String>,
+    image: Option<Image>
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "type")]
+enum Embed {
+    Website(Metadata),
+    Image(Media),
+    None
 }
 
 async fn embed(info: Query<Parameters>) -> Result<impl Responder, Error> {
     let url = info.into_inner().url;
 
-    let resp = reqwest::get(url).await.map_err(|_| Error::LabelMe)?;
+    let resp = reqwest::get(&url).await.map_err(|_| Error::LabelMe)?;
     if !resp.status().is_success() {
         return Err(Error::LabelMe);
     }
@@ -77,6 +76,7 @@ async fn embed(info: Query<Parameters>) -> Result<impl Responder, Error> {
         .ok_or_else(|| Error::LabelMe)?
         .to_str()
         .map_err(|_| Error::LabelMe)?;
+    
     let mine: mime::Mime = content_type.parse().map_err(|_| Error::LabelMe)?;
 
     if let mime::HTML = mine.subtype() {
@@ -84,35 +84,104 @@ async fn embed(info: Query<Parameters>) -> Result<impl Responder, Error> {
         let fragment = Html::parse_document(&body);
         let selector = Selector::parse("meta").map_err(|_| Error::LabelMe)?;
 
-        let mut properties = HashMap::<&str, String>::new();        
+        let mut properties = HashMap::<&str, &str>::new();
         for el in fragment.select(&selector) {
             let node = el.value();
 
             if let (Some(property), Some(content)) = (node.attr("property"), node.attr("content")) {
-                properties.insert(property, content.to_string());
+                properties.insert(property, content);
             }
         }
 
-        let mut metadata = Metadata { title: None };
-        metadata.title = properties.remove("og:title");
+        let mut metadata = Metadata::default();
+        metadata.title = properties
+            .remove("og:title")
+            .or_else(|| properties.remove("twitter:title"))
+            .or_else(|| properties.remove("title"))
+            .map(|v| v.to_string());
+        
+        metadata.description = properties
+            .remove("og:description")
+            .or_else(|| properties.remove("twitter:description"))
+            .or_else(|| properties.remove("description"))
+            .map(|v| v.to_string());
+        
+        metadata.image = properties.remove("og:image")
+            .or_else(|| properties.remove("twitter:image"))
+            .map(|url| {
+                let url = url.to_string();
+                let media = Media { url, width: 0, height: 0 };
 
-        if let Some(opengraph_type) = properties.remove("og:type") {
-            match &opengraph_type[..] {
-                "abc" => {},
-                _ => {}
+                match properties.remove("twitter:card") {
+                    Some("summary_large_image") => Image::Large(media),
+                    _ => Image::Preview(media)
+                }
+            });
+
+        if let Some(image) = &metadata.image {
+            if let Ok(image) = match image {
+                Image::Large( Media { url, .. } ) => fetch_media(MediaType::Large, url.to_string()),
+                Image::Preview( Media { url, .. } ) => fetch_media(MediaType::Preview, url.to_string())
+            }.await {
+                metadata.image = Some(image);
+            } else {
+                metadata.image = None;
             }
         }
-
-        Ok(format!("is html"))
+        
+        Ok(web::Json(Embed::Website(metadata)))
     } else if let mime::IMAGE = mine.type_() {
         let bytes = resp.bytes().await.map_err(|_| Error::LabelMe)?;
         if let Ok(size) = imagesize::blob_size(&bytes) {
-            dbg!(size);
-            Ok(format!("is image"))
+            let imagesize::ImageSize { width, height } = size;
+            let  width =  width as isize;
+            let height = height as isize;
+
+            let media = Media { url, width, height };
+            Ok(web::Json(Embed::Image(media)))
         } else {
-            Ok(format!("said it was an image but we couldn't parse it"))
+            Ok(web::Json(Embed::None))
         }
     } else {
-        Ok(format!("not html!!"))
+        Ok(web::Json(Embed::None))
     }
+}
+
+enum MediaType {
+    Large,
+    Preview
+}
+
+async fn fetch_media(media_type: MediaType, url: String) -> Result<Image, Error> {
+    let resp = reqwest::get(&url).await.map_err(|_| Error::LabelMe)?;
+    if !resp.status().is_success() {
+        return Err(Error::LabelMe);
+    }
+
+    let content_type = resp
+        .headers()
+        .get(CONTENT_TYPE)
+        .ok_or_else(|| Error::LabelMe)?
+        .to_str()
+        .map_err(|_| Error::LabelMe)?;
+    
+    let mine: mime::Mime = content_type.parse().map_err(|_| Error::LabelMe)?;
+    if let mime::IMAGE = mine.type_() {
+        let bytes = resp.bytes().await.map_err(|_| Error::LabelMe)?;
+        if let Ok(size) = imagesize::blob_size(&bytes) {
+            let imagesize::ImageSize { width, height } = size;
+            let  width =  width as isize;
+            let height = height as isize;
+
+            let media = Media { url, width, height };
+            return Ok(
+                match media_type {
+                    MediaType::Large => Image::Large(media),
+                    MediaType::Preview => Image::Preview(media)
+                }
+            )
+        }
+    }
+
+    Err(Error::LabelMe)
 }
