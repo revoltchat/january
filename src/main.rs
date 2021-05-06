a86570b35aae57ffafb1f9809b02de675c4ac6a5
Paul Makles <paulmakles@gmail.com>
2021-05-06 15:20:03 +0100

    Detect images, work on meta scraper.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web::{self, Query}};
+use std::collections::HashMap;
+
+use actix_web::{
+    web::{self, Query},
+    App, HttpRequest, HttpServer, Responder,
+};
+use reqwest::header::CONTENT_TYPE;
 use scraper::{Html, Selector};
-use serde::{Deserialize};
+use serde::Deserialize;
+
+pub mod util;
+use util::result::Error;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -19,7 +28,7 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-#[derive(Deserialize)]
+/*#[derive(Deserialize)]
 struct Info {
     url: String,
 }
@@ -29,6 +38,8 @@ async fn embed(info: Query<Info>) -> impl Responder {
 
     let resp = reqwest::get(url).await.unwrap();
     assert!(resp.status().is_success());
+
+    dbg!(resp.headers().get(CONTENT_TYPE));
 
     let body = resp.text().await.unwrap();
     let fragment = Html::parse_document(&body);
@@ -41,4 +52,67 @@ async fn embed(info: Query<Info>) -> impl Responder {
     }
 
     format!("gaming has been deposited into stdout")
+}*/
+
+#[derive(Deserialize)]
+struct Parameters {
+    url: String,
+}
+
+struct Metadata {
+    title: Option<String>,
+}
+
+async fn embed(info: Query<Parameters>) -> Result<impl Responder, Error> {
+    let url = info.into_inner().url;
+
+    let resp = reqwest::get(url).await.map_err(|_| Error::LabelMe)?;
+    if !resp.status().is_success() {
+        return Err(Error::LabelMe);
+    }
+
+    let content_type = resp
+        .headers()
+        .get(CONTENT_TYPE)
+        .ok_or_else(|| Error::LabelMe)?
+        .to_str()
+        .map_err(|_| Error::LabelMe)?;
+    let mine: mime::Mime = content_type.parse().map_err(|_| Error::LabelMe)?;
+
+    if let mime::HTML = mine.subtype() {
+        let body = resp.text().await.map_err(|_| Error::LabelMe)?;
+        let fragment = Html::parse_document(&body);
+        let selector = Selector::parse("meta").map_err(|_| Error::LabelMe)?;
+
+        let mut properties = HashMap::<&str, String>::new();        
+        for el in fragment.select(&selector) {
+            let node = el.value();
+
+            if let (Some(property), Some(content)) = (node.attr("property"), node.attr("content")) {
+                properties.insert(property, content.to_string());
+            }
+        }
+
+        let mut metadata = Metadata { title: None };
+        metadata.title = properties.remove("og:title");
+
+        if let Some(opengraph_type) = properties.remove("og:type") {
+            match &opengraph_type[..] {
+                "abc" => {},
+                _ => {}
+            }
+        }
+
+        Ok(format!("is html"))
+    } else if let mime::IMAGE = mine.type_() {
+        let bytes = resp.bytes().await.map_err(|_| Error::LabelMe)?;
+        if let Ok(size) = imagesize::blob_size(&bytes) {
+            dbg!(size);
+            Ok(format!("is image"))
+        } else {
+            Ok(format!("said it was an image but we couldn't parse it"))
+        }
+    } else {
+        Ok(format!("not html!!"))
+    }
 }
---
+++ src/util/result.rs
@@ -0,0 +1,53 @@
+use actix_web::{body::Body, http::StatusCode, BaseHttpResponse};
+use actix_web::{web::HttpResponse, ResponseError};
+use serde::Serialize;
+use serde_json;
+use std::fmt::Display;
+
+#[derive(Serialize, Debug)]
+#[serde(tag = "type")]
+pub enum Error {
+    /*FileTooLarge { max_size: usize },
+    FileTypeNotAllowed,
+    FailedToReceive,
+    DatabaseError,
+    MissingData,
+    UnknownTag,
+    ProbeError,
+    NotFound,
+    IOError,
+    S3Error,*/
+    LabelMe,
+}
+
+impl Display for Error {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl ResponseError for Error {
+    fn status_code(&self) -> StatusCode {
+        match &self {
+            /* Error::FileTooLarge { .. } => StatusCode::PAYLOAD_TOO_LARGE,
+            Error::FileTypeNotAllowed => StatusCode::BAD_REQUEST,
+            Error::FailedToReceive => StatusCode::BAD_REQUEST,
+            Error::DatabaseError => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::MissingData => StatusCode::BAD_REQUEST,
+            Error::UnknownTag => StatusCode::BAD_REQUEST,
+            Error::ProbeError => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::NotFound => StatusCode::NOT_FOUND,
+            Error::IOError => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::S3Error => StatusCode::INTERNAL_SERVER_ERROR, */
+            Error::LabelMe => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse<actix_web::dev::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::build(self.status_code())
+            .content_type("application/json")
+            .body(body)
+    }
+}
