2836a43da9c8a7124cb60702950fcd77ba0bb9fd
brecert <me@bree.dev>
2021-09-20 00:11:01 -0400

    Add basic logging
    - added basic logging, similar to autumn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate lazy_static;
 
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
+use log::info;
 use util::variables::HOST;
 
 pub mod routes;
@@ -10,8 +12,13 @@ pub mod util;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"));
+
+    info!("Starting January server.");
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .route("/", web::get().to(routes::info::get))
             .route("/embed", web::get().to(routes::embed::get))
             .route("/proxy", web::get().to(routes::proxy::get))
