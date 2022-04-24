260af7797d1e02834e82c1924470469f2e2f9168
bree <me@bree.dev>
2022-04-24 17:13:30 -0400

    feat: add dotenv support for better feature parity with the other projects

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ pub mod util;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
     env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"));
 
     info!("Starting January server.");
