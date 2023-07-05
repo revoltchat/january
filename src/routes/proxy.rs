1962846f7a05f186448a96bccb4e6250392e08cb
TheBobBobs <84781603+TheBobBobs@users.noreply.github.com>
2023-07-05 19:48:52 +0000

    feat: allow setting max bytes to proxy/embed

--- src/routes/proxy.rs
+++ src/routes/proxy.rs
@@ -1,7 +1,7 @@
 use actix_web::{web::Query, HttpResponse, Responder};
 use serde::Deserialize;
 
-use crate::util::request::fetch;
+use crate::util::request::{fetch, get_bytes};
 use crate::util::result::Error;
 
 #[derive(Deserialize)]
@@ -11,14 +11,11 @@ pub struct Parameters {
 
 pub async fn get(info: Query<Parameters>) -> Result<impl Responder, Error> {
     let url = info.into_inner().url;
-    let (resp, mime) = fetch(&url).await?;
+    let (mut resp, mime) = fetch(&url).await?;
 
     if matches!(mime.type_(), mime::IMAGE | mime::VIDEO) {
-        let body = resp
-            .bytes()
-            .await
-            .map_err(|_| Error::FailedToConsumeBytes)?;
-        Ok(HttpResponse::Ok().body(body))
+        let bytes = get_bytes(&mut resp).await?;
+        Ok(HttpResponse::Ok().body(bytes))
     } else {
         Err(Error::NotAllowedToProxy)
     }
